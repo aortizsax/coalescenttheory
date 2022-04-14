17cef4c35c3c763c2efa38dd9b309b4eec0eb0a4
aortizsax <58195477+aortizsax@users.noreply.github.com>
2022-04-14 14:55:36 -0700

    Add files via upload

---
+++ scripts/coalescent_0.0.0.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 20:20:50 2022
+
+@author: aorti
+"""
+import random
+import string
+
+### NODE
+
+class Node:
+    """
+    
+    """
+    def __init__(self,name=None, Tw=None):
+        self.name=name
+        self.Tw=Tw
+        self.parent=None
+        self.child1=None
+        self.child2=None
+        
+# =============================================================================
+#     def __str__(self):
+#         print(self.name)
+# =============================================================================
+
+# Coalesent tree 
+class Tree:
+    def __init__(self, fnode=None, gnode=None):
+        pass
+    #root
+
+
+N = 10000000000000000000000000#infinity 
+Twait = 10
+n0 = 26
+
+samplednodes = [] #samplede nodes
+
+for i in range(n0):
+    ab = string.ascii_lowercase[i]
+    samplednodes.append(Node(ab,Twait))
+    #print(samplednodes[i].name)
+
+while len(samplednodes)>2:
+    i+=1  
+    r1 = random.sample(range(0,len(samplednodes)-1),2)
+    #for n in samplednodes:
+        #print(n.name)
+    #print(r1,samplednodes[r1[0]].name, samplednodes[r1[1]].name)    
+    ab = '('+samplednodes[r1[0]].name + ',' + samplednodes[r1[1]].name+')'
+    #string.ascii_lowercase[i]
+    
+    coal1 = samplednodes[r1[0]]
+    coal2 = samplednodes[r1[1]]
+    
+    samplednodes.pop(samplednodes.index(coal1))
+    samplednodes.pop(samplednodes.index(coal2))
+    samplednodes.append(Node(ab,Twait))
+    i+=1
+
+print('('+samplednodes[0].name+','+samplednodes[1].name+')')
